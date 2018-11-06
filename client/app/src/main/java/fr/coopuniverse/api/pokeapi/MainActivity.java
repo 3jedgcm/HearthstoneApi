33b75686eebeb868a7835ded01c2b2629ac6d2d4
Unknown <clement.merlet@etu.univ-lyon1.fr>
2018-11-06 13:33:45 +0100

    [EXAMPLE] Add example project
    New example project to easy connect into facebook api and google

--- client/app/src/main/java/fr/coopuniverse/api/pokeapi/MainActivity.java
+++ client/app/src/main/java/fr/coopuniverse/api/pokeapi/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -29,6 +30,7 @@ import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.shobhitpuri.custombuttons.GoogleSignInButton;
 import com.squareup.picasso.Picasso;
 
 import org.json.JSONObject;
@@ -39,27 +41,30 @@ public class MainActivity extends AppCompatActivity {
 
     CallbackManager callbackManager;
     LoginButton loginButton;
-    public static AccessToken accessToken;
+    AccessToken accessToken;
     TextView id;
     TextView name;
     TextView mail;
     boolean isLoggedIn;
+    Button test;
     Button disconnect;
-    SignInButton signInButton;
+    GoogleSignInButton signInButton;
     GoogleSignInClient mGoogleSignInClient;
     ImageView imageView;
+
+
+    //https://cdn.shopify.com/s/files/1/0441/9405/files/Pokemon_Moon_phone_wallpaper_by_Trinket_Geek_large.jpg?v=1481926584
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         // Google connexion
-        signInButton = findViewById(R.id.sign_in_button);
-        this.id = findViewById(R.id.id);
-        this.mail= findViewById(R.id.mail);
-        this.name = findViewById(R.id.name);
-        this.imageView = findViewById(R.id.imageView);
+        signInButton = (GoogleSignInButton) findViewById(R.id.sign_in_button);
         this.disconnect = findViewById(R.id.button2);
-        signInButton.setSize(SignInButton.SIZE_STANDARD);
+
+
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
         disconnect.setEnabled(false);
@@ -68,21 +73,23 @@ public class MainActivity extends AppCompatActivity {
                 signIn();
             }
         });
+
+
+
         disconnect.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                mGoogleSignInClient.signOut()
-                        .addOnCompleteListener(MainActivity.this, new OnCompleteListener<Void>() {
-                            @Override
-                            public void onComplete(@NonNull Task<Void> task) {
-                                Log.d("Debug-Chaton","Bien deco");
-                                mail.setText("Mail : ");
-                                name.setText("Name : ");
-                                id.setText("Id : ");
-                                imageView.setVisibility(View.INVISIBLE);
-                                disconnect.setEnabled(false);
-                                signInButton.setEnabled(true);
-                            }
-                        });
+                mGoogleSignInClient.signOut().addOnCompleteListener(MainActivity.this, new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+
+                        mail.setText("Mail : ");
+                        name.setText("Name : ");
+                        id.setText("Id : ");
+                        imageView.setVisibility(View.INVISIBLE);
+                        disconnect.setEnabled(false);
+                        signInButton.setEnabled(true);
+                    }
+                });
             }
         });
 
@@ -93,11 +100,10 @@ public class MainActivity extends AppCompatActivity {
         this.callbackManager = CallbackManager.Factory.create();
         this.loginButton = (LoginButton) findViewById(R.id.login_button);
         this.loginButton.setReadPermissions("email");
-
         this.loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
-                getInfo();
+
             }
 
             @Override
@@ -112,8 +118,35 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+        callbackManager = CallbackManager.Factory.create();
+        LoginManager.getInstance().registerCallback(callbackManager,
+                new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+                        GraphRequest request = GraphRequest.newMeRequest(loginResult.getAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
+                            @Override
+                            public void onCompleted(JSONObject object, GraphResponse response) {
+                                Log.d("Chaton",object.toString());
+                            }
+                        });
+                        Bundle parameters = new Bundle();
+                        parameters.putString("fields", "id,name,link");
+                        request.setParameters(parameters);
+                        request.executeAsync();
+                    }
 
-        }
+                    @Override
+                    public void onCancel() {
+                        // App code
+                    }
+
+                    @Override
+                    public void onError(FacebookException exception) {
+                        // App code
+                    }
+                });
+    }
+
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         callbackManager.onActivityResult(requestCode, resultCode, data);
         super.onActivityResult(requestCode, resultCode, data);
@@ -123,34 +156,16 @@ public class MainActivity extends AppCompatActivity {
             // a listener.
 
 
-
             Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
             handleSignInResult(task);
 
         }
     }
 
-    private void getInfo()
-    {
-        GraphRequest request = GraphRequest.newMeRequest(MainActivity.accessToken, new GraphRequest.GraphJSONObjectCallback() {
-                    @Override
-                    public void onCompleted(JSONObject object, GraphResponse response) {
-                        if(object != null)
-                        Log.d("Debug-chaton",object.toString());
-
-                    }
-                });
-
-        Bundle parameters = new Bundle();
-        parameters.putString("fields", "id,name,link");
-        request.setParameters(parameters);
-        request.executeAsync();
-    }
 
     private void signIn() {
         Intent signInIntent = mGoogleSignInClient.getSignInIntent();
         startActivityForResult(signInIntent, 1);
-
 
 
     }
@@ -169,15 +184,16 @@ public class MainActivity extends AppCompatActivity {
                 String personId = acct.getId();
                 Uri personPhoto = acct.getPhotoUrl();
                 this.mail.setText("Mail : " + personEmail);
-                this.name.setText("Name : " + personName + " - " + personGivenName + " - " +  personFamilyName + " -");
+                this.name.setText("Name : " + personName + " - " + personGivenName + " - " + personFamilyName + " -");
                 this.id.setText("Id : " + personId);
                 this.signInButton.setEnabled(false);
-                if(personPhoto != null) {
+                if (personPhoto != null) {
                     Picasso.get().load(personPhoto).into(imageView);
                 }
             }
 
-        } catch (ApiException e) { }
+        } catch (ApiException e) {
+        }
     }
 
 }

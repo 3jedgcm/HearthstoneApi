23c221100158b714266deeca77bcfeeb1ea023a8
3jedgcm <clement.merlet26@gmail.com>
2019-01-24 01:21:34 +0100

    [FEAT] Feat get question

--- client_kotlin/app/src/main/java/fr/coopuniverse/api/pokeapi/activity/fragment/QuizzFragment.kt
+++ client_kotlin/app/src/main/java/fr/coopuniverse/api/pokeapi/activity/fragment/QuizzFragment.kt
@@ -10,12 +10,26 @@ import android.view.ViewGroup
 
 import fr.coopuniverse.api.pokeapi.R
 import fr.coopuniverse.api.pokeapi.activity.activity.CallBackDisplay
+import fr.coopuniverse.api.pokeapi.activity.activity.Destination
 import fr.coopuniverse.api.pokeapi.activity.httpRequestManager.CallBackGenerator
 import fr.coopuniverse.api.pokeapi.activity.httpRequestManager.Reponse
+import kotlinx.android.synthetic.main.quizz_fragment.*
 
 
 class QuizzFragment : androidx.fragment.app.Fragment() , CallBackDisplay {
     override fun display(rep: Reponse, action: String) {
+
+        answer.text = rep.data.question[0].toString()
+        var reps: ArrayList<String> = rep.data.question.get(1) as ArrayList<String>
+        reponse_one.isClickable = true;
+        reponse_two.isClickable = true;
+        reponse_three.isClickable = true;
+        reponse_four.isClickable = true;
+        reponse_one.text = reps[0]
+        reponse_two.text = reps[1]
+        reponse_three.text = reps[2]
+        reponse_four.text = reps[3]
+
         Log.d("Chaton",rep.toString())
     }
 
@@ -23,11 +37,18 @@ class QuizzFragment : androidx.fragment.app.Fragment() , CallBackDisplay {
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.quizz_fragment, container, false)
+
     }
 
-    fun onClick(v:View)
-    {
-        CallBackGenerator(callback = this,action = "GetQuestion",isActivateCallBack = true, url = "https://api.coopuniverse.fr/").execute()
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        reponse_one.isClickable = false
+        reponse_two.isClickable = false
+        reponse_three.isClickable = false
+        reponse_four.isClickable = false
+        went.setOnClickListener {
+            CallBackGenerator(callback = this,action = "GetQuestion",isActivateCallBack = true, url = "https://api.coopuniverse.fr/").execute()
+        }
     }
 
 

7cb3680234a3c77240f8ca9b69d459dc86314425
3jedgcm <clement.merlet@etu.univ-lyon1.fr>
2019-02-20 14:14:35 +0100

    [FIX] Fix enums packages

--- client_kotlin/app/src/main/java/fr/coopuniverse/api/pokeapi/activity/view/viewModel/ShopViewModel.kt
+++ client_kotlin/app/src/main/java/fr/coopuniverse/api/pokeapi/activity/view/viewModel/ShopViewModel.kt
@@ -14,7 +14,7 @@ import fr.coopuniverse.api.pokeapi.activity.data.Account
 import fr.coopuniverse.api.pokeapi.activity.data.Card
 import fr.coopuniverse.api.pokeapi.activity.data.Config
 import fr.coopuniverse.api.pokeapi.activity.data.Reponse
-import fr.coopuniverse.api.pokeapi.activity.enum.Route
+import fr.coopuniverse.api.pokeapi.activity.enums.Route
 import fr.coopuniverse.api.pokeapi.activity.manager.CallHttpManager
 import kotlinx.android.synthetic.main.inventory_fragment.*
 
